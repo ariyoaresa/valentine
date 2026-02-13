3b25d3ec593911c54024de9469235c6a4ae51df6
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-02-13 10:14:13 +0100

    Optimize images

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
     import QuestionaireCard from "./lib/QuestionaireCard.svelte"
+
+    const images = [
+        "/1.gif",
+        "/2.gif",
+        "/3.gif",
+        "/4.gif",
+        "/5.gif",
+        "/6.gif",
+    ];
+
+    images.forEach(src => {
+        const img = new Image();
+        img.src = src;
+    });
+
 </script>
 
 <main>
@@ -10,7 +25,7 @@
 main {
     display: grid;
     place-content: center;
-    background: url('/bg.png');
+    background: url('/bg.jpg');
     width: 100%;
     height: 100dvh;
 }
